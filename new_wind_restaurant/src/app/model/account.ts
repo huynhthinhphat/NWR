9795397d5a4be1eec69ffa2fe86884ebb6ea413e
Huỳnh Thịnh Phát <132771325+huynhthinhphat@users.noreply.github.com>
2024-06-25 19:09:23 +0700

    Delete new_wind_restaurant directory

--- new_wind_restaurant/src/app/model/account.ts
+++
@@ -1,8 +0,0 @@
-export class Account {
-    id: any
-    constructor(
-        public user: string = "",
-        public password: string = "",
-        public typeAccount: string = ""
-    ) { }
-}
